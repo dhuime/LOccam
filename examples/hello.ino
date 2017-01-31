ea9441b9db90d52b9d6ffe47801e6f7b2b7fa441
gilbert dhuime <gilbert.dhuime@orange.fr>
2017-01-31 16:27:56 +0100

    Add files via upload

--- examples/hello.ino
+++ examples/hello.ino
@@ -1,1 +1,16 @@
+#include <LOccam.h>
 
+PROC hello(void *pvParameters){
+  
+  Serial.println("Hello World !!")  ;
+  EXIT()          ; // terminate hello
+}
+
+void setup() {
+  Serial.begin(9600)  ;
+  
+  PAR(hello, NULL)      ; // declare hello without parameters
+  START()               ; // start hello
+}
+
+void loop(){}
